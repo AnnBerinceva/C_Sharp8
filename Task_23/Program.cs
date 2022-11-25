7913cb25747165237ed0f213c09d80f1ed3f985d
AnnBerinceva <berinceva92@bk.ru>
2022-11-25 10:57:16 +0500

    Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = ReadInt("Число N ");
+
+for (int a = 1; a <= num; a++)
+{
+    Console.WriteLine($"{a*a*a} "); 
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
