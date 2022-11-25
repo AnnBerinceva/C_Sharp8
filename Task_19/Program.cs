712008184479eadd0deb347d24ceb8758440d19d
AnnBerinceva <berinceva92@bk.ru>
2022-11-25 10:56:55 +0500

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int run = number.Length;
+if (run == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Является Палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine($"{number} - Не Является Палиндромом");
+    }
+}
